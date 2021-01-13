232ad0d82c8e2b2a2076003e54e40bdeac917ae3
Parsa <salehipour.edu@gmail.com>
2021-01-13 15:50:03 +0330

    calc_scores small changes

--- calc_scores_py.py
+++ calc_scores_py.py
@@ -3,27 +3,21 @@ import scipy
 from scipy.sparse import csr_matrix
 from sklearn.metrics.pairwise import cosine_similarity
 sparse_matrix = scipy.sparse.load_npz('./sparse_matrix.npz')
+sparse_matrix.eliminate_zeros()
 
 
-# print(sparse_matrix.getrow(8))
-# print()
-# print(sparse_matrix.getrow(278854))
 
-# print(sparse_matrix.nonzero())
 
-# print(corr)
-row_8 = sparse_matrix.getrow(254) 
 
-corr = cosine_similarity(row_8,sparse_matrix)
-top10 = np.argsort(corr[0])[-10:]
-print(cosine_similarity(row_8 ,sparse_matrix.getrow(40795) ))
-print(top10)
-# for i in range(10000):
-#     row = sparse_matrix.getrow(i+9) 
-#     # corr = cosine_similarity(sparse_matrix)
-#     # corr = np.corrcoef(row,row_8)
-#     corr = row_8.dot(row.transpose()).data
-#     if len(corr) > 0:
-        
-#         corr = cosine_similarity(row_8,row)
-#         print(f'rownum = {i+9} : correaltion = {corr}')
+# for i in range(27678611):
+for i in range(1,1000):
+    current_row = sparse_matrix.getrow(i) 
+    if(current_row.count_nonzero() > 0 ):
+        corr = cosine_similarity(current_row,sparse_matrix)
+        top10_indices = np.argsort(corr[0])[-11:-2]
+        (corr[0]).sort()
+        top10_values = (corr[0])[-11:-2]
+        # print(f'i = {i} : indices : {top10_indices}')
+        # print(f'          values : {top10_values}')
+
+
