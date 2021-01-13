799b591c323101e3e9185a29a3e30b2cb0762948
Ali <alijfri99@gmail.com>
2021-01-13 10:50:35 +0330

    Did some indexing on the database

---
+++ create_indices.sql
@@ -0,0 +1,5 @@
+use ir_assignment;
+create index idxISBN on books(ISBN);
+create index idxISBN on ratings(ISBN);
+create index idxID on users(`User-ID`);
+create index idxID on ratings(`User-ID`);
---
+++ drop_indices.sql
@@ -0,0 +1,5 @@
+use ir_assignment;
+drop index idxISBN on books;
+drop index idxISBN on ratings;
+drop index idxID on users;
+drop index idxID on ratings;
