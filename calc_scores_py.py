5450fb4caed7d567f74decaa37542880a063e756
Parsa <salehipour.edu@gmail.com>
2021-01-24 17:08:33 +0330

    model based collaborative filtering added

--- calc_scores_py.py
+++ calc_scores_py.py
@@ -7,5 +7,9 @@ sparse_matrix = scipy.sparse.load_npz('./sparse_matrix.npz')
 
 
 similarities_sparse = cosine_similarity(sparse_matrix , dense_output=False)
-
-print(similarities_sparse.getrow(8))
+similarities_sparse.sum_duplicates()
+similarities_sparse.eliminate_zeros()
+print(len(similarities_sparse.data))
+# print(len(similarities_sparse.nonzero()[1]))
+# print(similarities_sparse.nonzero()[1])
+# print(similarities_sparse.getrow(8))
