ff4fddff0ce0a6d51c6f11a4c2efcf08272cad8e
Parsa <salehipour.edu@gmail.com>
2021-01-30 21:52:09 +0330

    removed extra files

--- create_indices.sql
+++
@@ -1,5 +0,0 @@
-use ir_assignment;
-create index idxISBN on books(ISBN);
-create index idxISBN on ratings(ISBN);
--- create index idxID on users(`User-ID`);
--- create index idxID on ratings(`User-ID`);
